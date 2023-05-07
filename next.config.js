27a4a39ac5a59ada13481b5afe345e543b6e874c
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-05-08 02:28:55 +0900

    superjsonの導入

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withSuperjson } = require("next-superjson");
 
-module.exports = nextConfig
+module.exports = withSuperjson()({
+  reactStrictMode: true,
+});
